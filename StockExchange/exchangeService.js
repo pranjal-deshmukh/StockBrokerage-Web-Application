8c481b4349483d3b4636248d5d92fe9bbeec69bb
Trey Lewis <tdl190000@utdallas.edu>
2019-11-19 09:33:48 -0600

    backing up work

--- StockExchange/exchangeService.js
+++ StockExchange/exchangeService.js
@@ -9,23 +9,39 @@ const connection = mysql.createConnection({
     database : 'brokerage'
   });
 
+function stockPrice(stockSymbol, callback) {
+    connection.query(`SELECT Price As price FROM stocks WHERE Stock_Name = '${stockSymbol}'`, function (error, results, fields) {
+        if (error) throw error;
+        // console.log('The solution is: ', results[0].solution);
+        console.log('Query result: ', results);
+        // query_result = results;
+        return callback(results);
+    });
+}
+
 class ExchangeService {
 
     getStockPrice(stockSymbol) {
-        let query_result;
 
-        connection.connect();
-        connection.query(`SELECT Price As price FROM stocks WHERE Stock_Name = '${stockSymbol}'`, function (error, results, fields) {
-            if (error) throw error;
-            // console.log('The solution is: ', results[0].solution);
-            console.log('Query result: ', results);
-            return results;
+        // connection.connect();
+        // connection.query(`SELECT Price As price FROM stocks WHERE Stock_Name = '${stockSymbol}'`, function (error, results, fields) {
+        //     if (error) throw error;
+        //     // console.log('The solution is: ', results[0].solution);
+        //     console.log('Query result: ', results);
+        //     query_result = results;
+        //     return results;
+        // });
+        // connection.end();
+
+        let result = [];
+        stockPrice(stockSymbol, function(db_result) {
+            result = db_result;
         });
-        connection.end();
-        console.log(`query_result: ${query_result}`);
+
+        console.log(`query_result: ${result}`);
         let price = 25.25;//query_result[0].price;
         console.log(`Retrieving latest stock price for: ${stockSymbol} = ${price}`);
-        return {price: price};
+        return {price: price, db_res: result};
     }
 
 }
