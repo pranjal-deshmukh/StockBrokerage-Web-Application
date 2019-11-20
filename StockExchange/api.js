fc52da174886746c877c7d7ef147c73270d7294f
Trey Lewis <tdl190000@utdallas.edu>
2019-11-19 22:48:55 -0600

    stockExchange app can now query for the latest stock price or the price history of a stock

--- StockExchange/api.js
+++ StockExchange/api.js
@@ -2,14 +2,38 @@
 
 const express = require('express');
 const router = express.Router();
-const ExchangeService = require('./exchangeService');
 
-const exchangeService = new ExchangeService();
+const mysql = require('mysql');
 
-router.get('/getStockPrice', (req,res) => {
-    let result = exchangeService.getStockPrice(req.body.symbol);
-    res.send(result);
-    return result;
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'stocks',
+    password : 'WPL6314',
+    database : 'brokerage'
+  });
+
+router.get('/getLatestStockPrice', (req,res) => {
+    connection.query(`SELECT Price As price, Timestamp as timestamp FROM stocks WHERE Stock_Name = '${req.body.symbol}' ORDER BY Timestamp DESC Limit 1`,
+        function (error, results, fields) {
+            if (error) throw error;
+
+            console.log('Query result: ', results);
+
+            res.send(results[0]);
+            return results;
+    });
+});
+
+router.get('/getStockHistory', (req, res) => {
+    connection.query(`SELECT Price As price, Timestamp as timestamp FROM stocks WHERE Stock_Name = '${req.body.symbol}' ORDER BY Timestamp DESC`,
+        function (error, results, fields) {
+            if (error) throw error;
+
+            console.log('Query result: ', results);
+
+            res.send({history: results});
+            return results;
+    });
 });
 
 module.exports = router;
--- mysqlConnectionFix.sql
+++ mysqlConnectionFix.sql
@@ -1,2 +1,2 @@
-ALTER USER '<username>'@'localhost' IDENTIFIED WITH mysql_native_password BY '<password>';
+ALTER USER '<username>' IDENTIFIED WITH mysql_native_password BY '<password>';
 flush privileges;
