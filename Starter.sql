b82a09a9fe1b481c1b44a66eab0a449b22ced683
SK <42592361+SheetalK99@users.noreply.github.com>
2019-11-23 11:48:16 -0600

    Update Starter.sql
    Added all table definitions

--- Starter.sql
+++ Starter.sql
@@ -1,8 +1,5 @@
 create database Brokerage;
-
-
 use Brokerage;
-
 
 CREATE TABLE `brokerage`.`stocks` (
   `idStocks` INT NOT NULL AUTO_INCREMENT,
@@ -14,4 +11,39 @@ CREATE TABLE `brokerage`.`stocks` (
 
 
 
-select * from stocks;
+CREATE TABLE `brokerage`.`user` (
+  `idUser` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
+  `email_id` VARCHAR(20) NOT NULL,
+  `balance` DOUBLE NULL DEFAULT 0,
+  PRIMARY KEY (`idUser`));
+
+CREATE TABLE `brokerage`.`transaction` (
+  `idtransaction` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(45) NULL,
+  `stockid` VARCHAR(45) NULL,
+  `buy_sell_flag` VARCHAR(5) NULL,
+  `time` TIMESTAMP NULL,
+  `num_stocks` INT NULL,
+  PRIMARY KEY (`idtransaction`));
+  
+  
+  CREATE TABLE `brokerage`.`current_stocks` (
+  `userid` INT NOT NULL,
+  `stockid` INT NOT NULL,
+  `num_stocks` INT NULL,
+  `num_recur_days_sell` INT NULL DEFAULT -1,
+  `num_recur_days_buy` INT NULL DEFAULT -1,
+  `num_stocks_sell` INT NULL DEFAULT 0,
+  `num_stocks_buy` INT NULL DEFAULT 0,
+  PRIMARY KEY (`userid`, `stockid`));
+
+
+CREATE TABLE `brokerage`.`bank` (
+  `idbank` INT NOT NULL AUTO_INCREMENT,
+  `userid` INT NOT NULL,
+  `account_no` VARCHAR(20) NULL,
+  `routing_no` VARCHAR(20) NULL,
+  PRIMARY KEY (`idbank`, `userid`));
