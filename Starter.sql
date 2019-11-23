f5e02447934d877e597e93f9f5a45f2c94945628
SK <42592361+SheetalK99@users.noreply.github.com>
2019-11-23 13:37:49 -0600

    Update Starter.sql

--- Starter.sql
+++ Starter.sql
@@ -5,7 +5,7 @@ CREATE TABLE `brokerage`.`stocks` (
   `idStocks` INT NOT NULL AUTO_INCREMENT,
   `Stock_Name` VARCHAR(45) NULL,
   `Price` DOUBLE NULL,
-  `Timestamp` DATETIME NULL,
+  `Timestamp` DATETIME  DEFAULT CURRENT_TIMESTAMP,
   `Company` VARCHAR(100) NULL,
   PRIMARY KEY (`idStocks`));
 
