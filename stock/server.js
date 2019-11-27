3740dbb0b411700f407f987b3a66fe27cf0ecf21
SK <42592361+SheetalK99@users.noreply.github.com>
2019-11-26 22:25:00 -0600

    Update server.js

--- stock/server.js
+++ stock/server.js
@@ -1,3 +1,4 @@
+// server.js
 
 // set up ======================================================================
 // get all the tools we need
@@ -6,18 +7,13 @@ var app      = express();
 var port     = process.env.PORT || 8080;
 var passport = require('passport');
 var flash    = require('connect-flash');
-var mysql = require('mysql');
+
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var config = require('./config/database.js');
-
-// configuration ===============================================================
-configDB = config.url;
-var connection=mysql.createConnection({user:configDB.user,password:configDB.password})
-
+var configDB = require('./config/database.js');
 
 
 require('./config/passport')(passport); // pass passport for configuration
@@ -25,7 +21,8 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
@@ -34,6 +31,9 @@ app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secre
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(function(err, req, res, next) {
+    console.log(err);
+});
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
