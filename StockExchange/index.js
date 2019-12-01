d98b874b72d842a641f5609bbbec62232b73b694
lewistd <42876124+lewistd@users.noreply.github.com>
2019-12-01 14:32:56 -0600

    Update index.js

--- StockExchange/index.js
+++ StockExchange/index.js
@@ -6,7 +6,7 @@ const routes = require('./api');
 
 const app = express();
 
-const port = 8080;
+const port = 8081;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
