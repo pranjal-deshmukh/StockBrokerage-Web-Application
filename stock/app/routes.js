0ff31b88aae52e9eaa68b34a3c2607fc695d9df5
SK <42592361+SheetalK99@users.noreply.github.com>
2019-12-04 08:56:24 -0600

    Update routes.js
    Added routes for adding money, editing profile, balance display

--- stock/app/routes.js
+++ stock/app/routes.js
@@ -1,20 +1,65 @@
+// load up the user model
+var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+var dbconfig = require('../config/database');
+var connection = mysql.createConnection(dbconfig.connection);
+
+connection.query('USE ' + dbconfig.database);
 module.exports = function (app, passport) {
 	var axios = require('axios');
 	var moment = require('moment');
-	var queries = require('./queries');
+	//var queries = require('./queries');
 
 	// normal routes ===============================================================
 
 
 	app.post('/add_bank_account', isLoggedIn, (req, res) => {
-		queries.add_bank_account(req.user.idUser, req.body.bank_acc_no, req.body.routing),
-			function (error, results, fields) {
-				if (error) throw error;
+		connection.query("Insert into bank(`userid`,`account_no`,`routing_no`) values('" +req.user.idUser+ "','" +req.body.bank_acc_no+ "','" +req.body.routing + "')",
+		function (error, results, fields) {
+			if (error) throw error;
+	
+			console.log('Inserting bank details');
+	
+			res.redirect('/profile');
+			
+		});
+	});
 
-				console.log('Account add successful');
-				res.redirect('/profile');
 
-			}
+	app.post('/update_profile', isLoggedIn, (req, res) => {
+		connection.query("update users set `address`='" +req.body.address+ "',`email`='" +req.body.email+ "' where `idUser`='" +req.user.idUser+"'",
+		function (error, results, fields) {
+			if (error) throw error;
+	
+			console.log('Updating profile');
+	
+			res.redirect('/profile');
+			
+		});
+	});
+
+	app.post('/add_money', isLoggedIn, (req, res) => {
+		connection.query("update users set `balance`=`balance`+'" +req.body.balance+ "' where `idUser`='" +req.user.idUser+"'",
+		function (error, results, fields) {
+			if (error) throw error;
+	
+			console.log('Updating balance');
+	
+			res.redirect('/profile');
+			
+		});
+	});
+
+	app.post('/update_schedule', isLoggedIn, (req, res) => {
+		connection.query("update users set `balance`=`balance`+''" +req.body.balance+ "where `idUser=''" +req.user.idUser,
+		function (error, results, fields) {
+			if (error) throw error;
+	
+			console.log('Updating balance');
+	
+			res.redirect('/profile');
+			
+		});
 	});
 
 	// show the home page (will also have our login links)
@@ -23,11 +68,42 @@ module.exports = function (app, passport) {
 	});
 
 	// PROFILE SECTION =========================
-	app.get('/profile', isLoggedIn, function (req, res) {
-		res.render('profile.ejs', {
-			user: req.user
+
+
+	app.get('/profile', function (req, res) {
+		return_data = {}
+		const id=req.user.idUser;
+		query1 = "select username, address, email, balance from users where `idUser`=" +id;
+		query2 = "select account_no from bank where `userid`=" +id;
+		connection.query(query1, {}, function(err, results) {
+			profile={
+                username:results[0].username,
+                address:results[0].address,
+                email:results[0].email,
+                balance:results[0].balance
+            };
+			return_data.user = profile;
+			//console.log(return_data);
+			
+			connection.query(query2, {}, function(err, results) {
+				var accounts=[];
+				console.log(results);
+				for (let i = results.length - 1; i >= 0; i--) {
+					accounts.push(results[i].account_no);
+				}
+				bank={
+					account_nos:accounts
+				};
+				return_data.banks = bank;
+				console.log(return_data);
+				res.render('profile.ejs', return_data);
+			
+				
+			});
 		});
 	});
+
+
 
 	// STOCK SEARCH =========================
 	app.get('/search', isLoggedIn, function (req, res) {
