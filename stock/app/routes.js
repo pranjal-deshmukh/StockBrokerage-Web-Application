1270366ace8e050734fa71d2bd702c3ee512b525
Trey Lewis <tdl190000@utdallas.edu>
2019-12-03 18:45:45 -0600

    various additions and changes

--- stock/app/routes.js
+++ stock/app/routes.js
@@ -1,40 +1,37 @@
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
 	var axios = require('axios');
 	var moment = require('moment');
-	var queries  = require('./queries');
+	var queries = require('./queries');
 
-// normal routes ===============================================================
+	// normal routes ===============================================================
 
 
-app.post('/add_bank_account',isLoggedIn, (req, res) => {
-	queries.add_bank_account(req.user.idUser,req.body.bank_acc_no,req.body.routing),
-        function (error, results, fields) {
-            if (error) throw error;
+	app.post('/add_bank_account', isLoggedIn, (req, res) => {
+		queries.add_bank_account(req.user.idUser, req.body.bank_acc_no, req.body.routing),
+			function (error, results, fields) {
+				if (error) throw error;
 
-			console.log('Account add successful');
-			res.redirect('/profile');
-			
-		}
-    });
+				console.log('Account add successful');
+				res.redirect('/profile');
+
+			}
+	});
 
 	// show the home page (will also have our login links)
-	app.get('/', function(req, res) {
+	app.get('/', function (req, res) {
 		res.render('index.ejs');
 	});
 
 	// PROFILE SECTION =========================
-	app.get('/profile', isLoggedIn, function(req, res) {
+	app.get('/profile', isLoggedIn, function (req, res) {
 		res.render('profile.ejs', {
-			user : req.user
+			user: req.user
 		});
 	});
 
 	// STOCK SEARCH =========================
-	app.get('/search', isLoggedIn, function(req, res) {
-		// var searchStock = require('./searchstock');
-		res.render('search.ejs', {
-			utils: 'hi'//searchStock
-		});
+	app.get('/search', isLoggedIn, function (req, res) {
+		res.render('search.ejs');
 	});
 
 	// SHOW STOCK PRICES
@@ -43,12 +40,10 @@ app.post('/add_bank_account',isLoggedIn, (req, res) => {
 		var time = req.params.time;
 
 		var date = moment();
-												  
 
 		if (time === 'wk') {
 			time = 'This Week';
 			date = date.startOf('week');
-											 
 		} else if (time === '1wk') {
 			time = 'Past Week';
 			date = date.subtract(7, 'days');
@@ -62,7 +57,7 @@ app.post('/add_bank_account',isLoggedIn, (req, res) => {
 			time = 'Past 5 Years';
 			date = date.subtract(5, 'years');
 		}
-		console.log(`${time} ISO: ` + date.toISOString);
+		console.log(`${time} ISO: ` + date.toISOString());
 		//get price info from exchange app.
 		function getCurrentPrice() {
 			const url = 'http://localhost:8081/api/getLatestStockPrice';
@@ -93,102 +88,102 @@ app.post('/add_bank_account',isLoggedIn, (req, res) => {
 			console.log('curr: ', result[0].data);
 			console.log('history: ', result[1].data);
 
+			let prices = [];
+			let dates = [];
+
+			for (let i = result[1].data.history.length - 1; i >= 0; i--) {
+				prices.push(result[1].data.history[i].price.toFixed(2));
+				dates.push(moment(result[1].data.history[i].timestamp).format('MM-DD-YYYY'));
+			}
+
 			res.render('showstock.ejs', {
-				utils: { stock, time, curr: result[0].data.price, history: result[1].data.history }
+				utils: { stock, company: result[0].data.company, time, curr: result[0].data.price.toFixed(2), prices, dates }
 			});
 		}).catch(err => console.log(err));
 
 
-		
 	});
 
 	// LOGOUT ==============================
-	app.get('/logout', function(req, res) {
+	app.get('/logout', function (req, res) {
 		req.logout();
 		res.redirect('/');
 	});
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+	// =============================================================================
+	// AUTHENTICATE (FIRST LOGIN) ==================================================
+	// =============================================================================
 
 	// locally --------------------------------
-		// LOGIN ===============================
-		// show the login form
-		app.get('/login', function(req, res) {
-			res.render('login.ejs', { message: req.flash('loginMessage') });
-		});
-	  
-														  
-																											
-																					 
-											
-	 
+	// LOGIN ===============================
+	// show the login form
+	app.get('/login', function (req, res) {
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
 
-											
-		// process the login form
-		app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/login', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+	// process the login form
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile', // redirect to the secure profile section
+		failureRedirect: '/login', // redirect back to the signup page if there is an error
+		failureFlash: true // allow flash messages
+	}));
 
-		// SIGNUP =================================
-		// show the signup form
-		app.get('/signup', function(req, res) {
-			res.render('signup.ejs', { message: req.flash('signupMessage') });
-		});
+	// SIGNUP =================================
+	// show the signup form
+	app.get('/signup', function (req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
 
-		// process the signup form
-		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/signup', // redirect back to the signup page if there is an error
-			
-			failureFlash : true // allow flash messages
-		}));
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect: '/profile', // redirect to the secure profile section
+		failureRedirect: '/signup', // redirect back to the signup page if there is an error
+
+		failureFlash: true // allow flash messages
+	}));
 
 
 
-// =============================================================================
-// AUTHORIZE (ALREADY LOGGED IN /  =============
-// =============================================================================
+	// =============================================================================
+	// AUTHORIZE (ALREADY LOGGED IN /  =============
+	// =============================================================================
 
 	// locally --------------------------------
-		app.get('/connect/local', function(req, res) {
-			res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-		});
-		app.post('/connect/local', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
-			failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-			failureFlash : true // allow flash messages
-		}));
+	app.get('/connect/local', function (req, res) {
+		res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+	});
+	app.post('/connect/local', passport.authenticate('local-signup', {
+		successRedirect: '/profile', // redirect to the secure profile section
+		failureRedirect: '/connect/local', // redirect back to the signup page if there is an error
+		failureFlash: true // allow flash messages
+	}));
 
 
 
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
+	// =============================================================================
+	// UNLINK ACCOUNTS =============================================================
+	// =============================================================================
+	// used to unlink accounts. for social accounts, just remove the token
+	// for local account, remove email and password
+	// user account will stay active in case they want to reconnect in the future
 
 	// local -----------------------------------
-	app.get('/unlink/local', isLoggedIn, function(req, res) {
-		var user            = req.user;
-		user.local.email    = undefined;
+	app.get('/unlink/local', isLoggedIn, function (req, res) {
+		var user = req.user;
+		user.local.email = undefined;
 		user.local.password = undefined;
-		user.save(function(err) {
+		user.save(function (err) {
 			res.redirect('/profile');
 		});
 	});
 
-										  
-							
-	
+	app.get('/invalid', function (req, res) {
+		res.render('invalid.ejs');
+	});
 
-							 
-						  
-	
+	app.use(function (req, res) {
+		res.redirect('/invalid')
+	});
 
 
 };
